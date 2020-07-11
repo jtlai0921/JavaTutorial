51fe0fcd8991a28819e87e496504a3912748d435
賴紀廷 <49492695+jtlai0921@users.noreply.github.com>
2020-07-11 13:10:48 +0800

    1

--- example/A/HelloWorld/src/HelloWorld.java
+++ example/A/HelloWorld/src/HelloWorld.java
@@ -1,6 +1,5 @@
-package onlyfun.caterpillar;
 
-public class HelloWorld { 
+public class HelloWorld {
     public static void main(String[] args) { 
         System.out.println("Hello Java!!"); 
     } 
--- example/CH3/HelloJava.java
+++ example/CH3/HelloJava.java
@@ -1,3 +1,5 @@
+package CH3;
+
 public class HelloJava {
     public static void main(String[] args) {
         System.out.println("嗨！我的第一個Java程式！");
